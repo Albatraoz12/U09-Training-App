c0e0c56a7fd893fa2a68aa5cf7f9fab3c467a6c8
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-16 20:19:02 +0200

    create user model

---
+++ backend/model/user.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      max: 50,
+    },
+    roll: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
