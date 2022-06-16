0a7b255085786e15549127fc3086f3156d36a55a
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-16 20:24:52 +0200

    register user function created, not tested yet

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,3 +3,21 @@ const router = express.Router();
 const dotenv = require("dotenv").config();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+
+//@desc Register A User
+//@routes POST /user/register
+//@access Public
+router.post("/register", async (req, res) => {
+  try {
+    const user = new User(req.body);
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    user.save().then(() => {
+      res.status(200).json({ message: "New user has been created! 👏" });
+    });
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
+});
