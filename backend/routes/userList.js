ffebb97b19083dc7cb59b18b910d22a468e63e32
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-17 14:29:33 +0200

    changed place for get userlist route and added comment

--- backend/routes/userList.js
+++ backend/routes/userList.js
@@ -4,16 +4,6 @@ const userList = require("../model/userList");
 const dotenv = require("dotenv").config();
 const router = express.Router();
 
-router.get("/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const list = await userList.find({ user: id });
-    res.status(200).json({ message: list });
-  } catch (error) {
-    res.status(404).json({ message: "There is no user in the system" });
-  }
-});
-
 //Crate userList
 router.post("/createList", (req, res) => {
   try {
@@ -26,6 +16,17 @@ router.post("/createList", (req, res) => {
       .then(res.status(200).json({ message: "new list has been created" }));
   } catch (error) {
     console.log(error);
+  }
+});
+
+//Get userlists by userID
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const list = await userList.find({ user: id });
+    res.status(200).json({ message: list });
+  } catch (error) {
+    res.status(404).json({ message: "There is no user in the system" });
   }
 });
 
