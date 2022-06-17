e4107976ca3b362bc2acf44491263bedd3f5c738
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-17 11:02:48 +0200

    test develoment branch

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React, It's fun!
+          Learn React, It's fun! VERRY FUN !! :D
         </a>
       </header>
     </div>
