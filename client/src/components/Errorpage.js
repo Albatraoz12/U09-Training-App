09a3700d236cddc5855aa19c9793947efbd93b10
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-17 18:58:12 +0200

    create components for each route and rendering each component on a url

---
+++ client/src/components/Errorpage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Errorpage = () => {
+  return (
+    <div>
+      Error Page not found! <a href="/">Go Back here</a>{" "}
+    </div>
+  );
+};
+
+export default Errorpage;
