99267c1f8c535d92d4e95480e6eac9617d6e485f
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-17 15:45:24 +0200

    create post route for create user info

--- backend/routes/userListInfo.js
+++ backend/routes/userListInfo.js
@@ -2,3 +2,6 @@ const express = require("express");
 const userListInfo = require("../model/userListInfo");
 const dotenv = require("dotenv").config();
 const router = express.Router();
+
+//Create ListInfo
+router.post("/createInfo", (req, res) => {});
