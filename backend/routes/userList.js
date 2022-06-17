8fe316870a292cabfd1afbd2cae5df2776f7dd0a
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-17 13:10:35 +0200

    create route to let users create a list

--- backend/routes/userList.js
+++ backend/routes/userList.js
@@ -3,3 +3,20 @@ const router = express.Router();
 const dotenv = require("dotenv").config();
 const User = require("../model/user");
 const userList = require("../model/userList");
+
+//Crate userList
+router.post("/createList", (req, res) => {
+  try {
+    const newList = new userList({
+      title: req.body.title,
+      user: req.body.user,
+    });
+    newList
+      .save()
+      .then(res.status(200).json({ message: "new list has been created" }));
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const userRouter = require("./routes/user");
-const userListRouter = require("./routes/userList");
+const listRouter = require("./routes/userList");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 8081;
 const app = express();
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/user", userRouter);
-app.use("/userList", userListRouter);
+app.use("/userList", listRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello There! - Obi-Wan, aaahhhh general kanobi!");
