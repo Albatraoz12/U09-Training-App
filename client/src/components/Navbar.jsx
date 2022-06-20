0aa70e6534fe23ac7c09d1a8cf48f3f7a346ac21
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-20 20:53:24 +0200

    create functionall navbar with styling and routing

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">
+                    Navbar
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/">
+                                Home
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/signin">
+                                Sign In
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/signup">
+                                Sign Up
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
