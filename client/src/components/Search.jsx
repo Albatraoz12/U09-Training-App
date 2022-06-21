6884d206d42afecba957f15cffbe164caea0f128
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-21 17:55:06 +0200

    create input in search component with hidden label for accessibility

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 
 function Search() {
-    return <div>Search</div>
+    return (
+        <main>
+            <section className="container mx-auto my-5">
+                <h1>Search</h1>
+                <form className="row g-3 mt-2">
+                    <div className="col-12">
+                        <label htmlFor="search" className="form-label hidden">
+                            Search Exercise
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="search"
+                            name="search"
+                            placeholder="Enter a exercise name"
+                        />
+                    </div>
+                </form>
+            </section>
+        </main>
+    )
 }
 
 export default Search
