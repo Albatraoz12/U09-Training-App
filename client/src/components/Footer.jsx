bdd5603690d3717c6eaa3749ebc341e7dcc0ff6f
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-21 20:06:54 +0200

    create footer component and some html

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <section>
+                <div>
+                    <h1 className="mt-3 fs-6 text-decoration-underline">Socials</h1>
+                    <img src="" alt="" />
+                </div>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer
