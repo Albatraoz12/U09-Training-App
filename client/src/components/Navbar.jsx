cf5e605a3c0e092a29f7a38201523645e80a42d2
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-21 17:45:40 +0200

    change home to search and navigate to /search in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -23,8 +23,8 @@ function Navbar() {
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">
-                                Home
+                            <a className="nav-link active" aria-current="page" href="/search">
+                                Search
                             </a>
                         </li>
                         <li className="nav-item">
