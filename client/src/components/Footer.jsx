aa1ae4afa22c5d0886605ec937feace2faa1dbc7
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-21 20:28:39 +0200

    create footer fixed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,13 +2,39 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer>
-            <section>
-                <div>
-                    <h1 className="mt-3 fs-6 text-decoration-underline">Socials</h1>
-                    <img src="" alt="" />
-                </div>
-            </section>
+        <footer className="text-center text-white">
+            <div className="container mt-5">
+                <section className="">
+                    {/* Instagram */}
+                    <a
+                        className="btn btn-outline-light btn-floating m-1"
+                        href="https://www.instagram.com/androomeda12/"
+                        role="button"
+                    >
+                        <i className="bi bi-instagram" />
+                    </a>
+                    {/* LinkedIn */}
+                    <a
+                        className="btn btn-outline-light btn-floating m-1"
+                        href="https://www.linkedin.com/in/dimosthenis-emmanouil-4ba731207/"
+                        role="button"
+                    >
+                        <i className="bi bi-linkedin" />
+                    </a>
+                    {/* GitHub */}
+                    <a
+                        className="btn btn-outline-light btn-floating m-1"
+                        href="https://github.com/Albatraoz12"
+                        role="button"
+                    >
+                        <i className="bi bi-github" />
+                    </a>
+                    <h1 className="mt-3 fs-4 text-decoration-underline">Socials</h1>
+                </section>
+                <section>
+                    <h2 className="f2-4">&copy; Dimosthenis Emmanouil</h2>
+                </section>
+            </div>
         </footer>
     )
 }
