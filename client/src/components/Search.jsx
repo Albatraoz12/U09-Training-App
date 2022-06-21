968d64c84df9235b1bdb2ff09676d6a0ef241e6a
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-21 18:03:37 +0200

    create select option in search component

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -18,6 +18,17 @@ function Search() {
                             placeholder="Enter a exercise name"
                         />
                     </div>
+                    <div className="col-12">
+                        <select
+                            className="form-select form-select-sm mb-3"
+                            aria-label=".form-select-lg example"
+                        >
+                            <option defaultValue>Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
                 </form>
             </section>
         </main>
