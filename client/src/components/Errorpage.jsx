c3334ad631cb2fe6328241312007962f66d8b475
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-22 14:54:52 +0200

    fix spacing in error component

--- client/src/components/Errorpage.jsx
+++ client/src/components/Errorpage.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 function Errorpage() {
     return (
-        <main className="container my-5 py-5">
-            <section className="d-flex justify-content-center align-items-start flex-column py-5">
+        <main className="container my-5">
+            <section className="d-flex justify-content-center align-items-start flex-column py-5 gap-4">
                 <h1>Oooops!</h1>
                 <p>
                     We can&apos;t seem to find the <br />
                     page you&apos;re looking for.
                 </p>
 
-                <p className="text-danger">Error code: 404</p>
+                <p>Error code: 404</p>
 
                 <a href="/" className="btn btn-primary">
                     Click Here to go back come
