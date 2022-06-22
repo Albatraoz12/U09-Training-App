cf70524bd0820f4da783326edde512d0de03b396
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-22 20:57:46 +0200

    switch onLogout from sign in to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import Cookies from 'js-cookie'
+import axios from 'axios'
 import logo from '../assets/logo-test.png'
 
 function Navbar() {
     const user = Cookies.get('access_token')
+    const onLogout = async () => {
+        try {
+            // Send token info in headers to backend to let user logout. Backend will remove HTTPOnly cookies
+            await axios
+                .get(`${process.env.REACT_APP_API_URL}user/signout`, {
+                    withCredentials: true,
+                    headers: {
+                        Authorization: `Bearer ${user}`,
+                    },
+                })
+                .then((res) => {
+                    if (res) {
+                        // FrontEnd removed access_token from cookies("localstorage").
+                        Cookies.remove('access_token')
+                        // eslint-disable-next-line no-console
+                        console.log('Success: successfully Logged Out!')
+                    }
+                })
+        } catch (error) {
+            // eslint-disable-next-line no-console
+            console.log(error)
+        }
+    }
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark">
@@ -42,7 +66,11 @@ function Navbar() {
                                     </a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link fw-bold text-white" href="/login">
+                                    <a
+                                        className="nav-link fw-bold text-white"
+                                        href="/signin"
+                                        onClick={onLogout}
+                                    >
                                         logout
                                     </a>
                                 </li>
