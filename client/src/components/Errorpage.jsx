a90b05bf1f3f7c62a78fe209ce057efa7c8554e3
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-22 14:52:09 +0200

    create basic error layout

--- client/src/components/Errorpage.jsx
+++ client/src/components/Errorpage.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 function Errorpage() {
     return (
-        <div>
-            Error Page not found! <a href="/">Go Back here</a>{' '}
-        </div>
+        <main className="container my-5 py-5">
+            <section className="d-flex justify-content-center align-items-start flex-column py-5">
+                <h1>Oooops!</h1>
+                <p>
+                    We can&apos;t seem to find the <br />
+                    page you&apos;re looking for.
+                </p>
+
+                <p className="text-danger">Error code: 404</p>
+
+                <a href="/" className="btn btn-primary">
+                    Click Here to go back come
+                </a>
+            </section>
+        </main>
     )
 }
 
