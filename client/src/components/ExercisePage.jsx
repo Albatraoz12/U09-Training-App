4ce87ac1c36a8abdb8926c257cc65e489601d035
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-23 11:10:55 +0200

    create exercisePage with some html

---
+++ client/src/components/ExercisePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ExercisePage() {
+    return (
+        <main>
+            <section>
+                <img src="http://d205bpvrqc9yn1.cloudfront.net/0024.gif" alt="Exercise Gif" />
+            </section>
+        </main>
+    )
+}
+
+export default ExercisePage
