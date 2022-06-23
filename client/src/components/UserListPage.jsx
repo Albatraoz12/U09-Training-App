93054dbab6a781f205ad9d53db621d2301ef86c7
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-23 14:50:53 +0200

    add button for later let user delete an item in list

--- client/src/components/UserListPage.jsx
+++ client/src/components/UserListPage.jsx
@@ -6,20 +6,35 @@ function UserListPage() {
             <section className="my-5 py-5 ">
                 <h1>List Title</h1>
                 <ul className='className="d-flex justify-content-center flex-column list-unstyled gap-2'>
-                    <li>
+                    <li className="d-flex justify-content-center align-items-center gap-2">
                         <a className="text-white" href="/exercise/1">
                             Exercise 1
                         </a>
+                        <button
+                            type="button"
+                            className="bi bi-x-lg btn btn-danger"
+                            aria-label="remove item"
+                        />
                     </li>
-                    <li>
+                    <li className="d-flex justify-content-center align-items-center gap-2">
                         <a className="text-white" href="/exercise/1">
                             Exercise 2
                         </a>
+                        <button
+                            type="button"
+                            className="bi bi-x-lg btn btn-danger"
+                            aria-label="remove item"
+                        />
                     </li>
-                    <li>
+                    <li className="d-flex justify-content-center align-items-center gap-2">
                         <a className="text-white" href="/exercise/1">
                             Exercise 3
                         </a>
+                        <button
+                            type="button"
+                            className="bi bi-x-lg btn btn-danger"
+                            aria-label="remove item"
+                        />
                     </li>
                 </ul>
             </section>
