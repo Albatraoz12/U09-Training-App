4ec40b092dfd8554cf7530127ac230baae5d187f
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-23 12:05:19 +0200

    create new page for User List view and new route to the page

---
+++ client/src/components/UserListPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function UserListPage() {
+    return (
+        <main>
+            <section>
+                <h1>hejsan</h1>
+            </section>
+        </main>
+    )
+}
+
+export default UserListPage
