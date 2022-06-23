fdbdc1dec90e752a5862d2d5447aaed4d48337de
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-23 12:10:53 +0200

    created list view layout with functional a tag href to exercise

--- client/src/components/UserListPage.jsx
+++ client/src/components/UserListPage.jsx
@@ -3,8 +3,25 @@ import React from 'react'
 function UserListPage() {
     return (
         <main>
-            <section>
-                <h1>hejsan</h1>
+            <section className="my-5 py-5 ">
+                <h1>List Title</h1>
+                <ul className='className="d-flex justify-content-center flex-column list-unstyled gap-2'>
+                    <li>
+                        <a className="text-white" href="/exercise/1">
+                            Exercise 1
+                        </a>
+                    </li>
+                    <li>
+                        <a className="text-white" href="/exercise/1">
+                            Exercise 2
+                        </a>
+                    </li>
+                    <li>
+                        <a className="text-white" href="/exercise/1">
+                            Exercise 3
+                        </a>
+                    </li>
+                </ul>
             </section>
         </main>
     )
