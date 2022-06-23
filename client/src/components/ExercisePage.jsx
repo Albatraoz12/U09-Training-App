8f5bce254222a5a44f5608abdc2564d72338fe63
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-23 11:36:47 +0200

    add article with text to represent the layout

--- client/src/components/ExercisePage.jsx
+++ client/src/components/ExercisePage.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 function ExercisePage() {
     return (
-        <main>
-            <section>
-                <img src="http://d205bpvrqc9yn1.cloudfront.net/0024.gif" alt="Exercise Gif" />
+        <main className="my-5">
+            <section className="container">
+                <h1>Exercise Name</h1>
+                <article className="px-5">
+                    <h2>About</h2>
+                    <p className="px-2">
+                        Exercise can increase your blood flow, try this exercise to strengthen your
+                        legs
+                    </p>
+                </article>
+                <img
+                    className="card-img-top"
+                    src="http://d205bpvrqc9yn1.cloudfront.net/0024.gif"
+                    alt="Exercise Gif"
+                />
             </section>
         </main>
     )
