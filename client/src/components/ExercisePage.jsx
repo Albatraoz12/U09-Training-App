81f30b793d459128cbb59d602bd7a6e01998b165
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-27 21:09:06 +0200

    fetch all users lists and saved if user is logged in

--- client/src/components/ExercisePage.jsx
+++ client/src/components/ExercisePage.jsx
@@ -8,11 +8,53 @@ function ExercisePage() {
     const user = Cookies.get('access_token')
     const [exercise, setExercise] = useState([])
     const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [getUser, setGetUser] = useState([])
+    const [getUserList, setGetUserList] = useState([])
+    const [getUserSaves, setGetUserSaves] = useState([])
 
     useEffect(() => {
-        if (user) {
-            setIsLoggedIn(true)
+        const checkUser = async () => {
+            // User sends its access_token in headers to BE to be decoded.
+            await axios
+                .get(`${process.env.REACT_APP_API_URL}user/protected`, {
+                    withCredentials: true,
+                    headers: {
+                        Authorization: `Bearer ${user}`,
+                    },
+                })
+                .then((res) => {
+                    if (res.data.user) {
+                        // Stores user info into the state.
+                        setGetUser(res.data.user)
+                    }
+                })
         }
+        const getLists = async () => {
+            await axios
+                .get(`${process.env.REACT_APP_API_URL}userList/${getUser.id}`, {
+                    withCredentials: true,
+                })
+                .then((res) => {
+                    if (res.data.message) {
+                        // Stores user info into the state.
+                        setGetUserList(res.data.message)
+                    }
+                })
+        }
+
+        const getSaves = async () => {
+            await axios
+                .get(`${process.env.REACT_APP_API_URL}userSaves/saves/${getUser.id}`, {
+                    withCredentials: true,
+                })
+                .then((res) => {
+                    if (res.data.sInfo) {
+                        // Stores user info into the state.
+                        setGetUserSaves(res.data.sInfo)
+                    }
+                })
+        }
+
         const options = {
             method: 'GET',
             url: `https://exercisedb.p.rapidapi.com/exercises/exercise/${params.id}`,
@@ -35,8 +77,17 @@ function ExercisePage() {
                 })
         }
 
+        if (user) {
+            setIsLoggedIn(true)
+            checkUser()
+            if (getUser.id) {
+                getLists()
+                getSaves()
+            }
+        }
+
         getExercise()
-    }, [params.id])
+    }, [params.id, user, getUser.id])
     return (
         <main className="my-5">
             <section className="container">
