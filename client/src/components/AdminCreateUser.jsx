5845d8cf89d636198370d4049961159f81b118dd
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-29 13:22:05 +0200

    create validation for AdminCreateUser route, if role is not admin the user will see ErrorPage component

--- client/src/components/AdminCreateUser.jsx
+++ client/src/components/AdminCreateUser.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import { useNavigate } from 'react-router-dom'
+import ErrorPage from './Errorpage'
 
 function AdminCreateUser() {
-    return <div>AdminCreateUser</div>
+    const navigate = useNavigate()
+    const user = Cookies.get('access_token')
+    const [getUser, setGetUser] = useState([])
+    const [isRole, setIsRole] = useState(Boolean)
+
+    useEffect(() => {
+        const checkUser = async () => {
+            // User sends its access_token in headers to BE to be decoded.
+            await axios
+                .get(`${process.env.REACT_APP_API_URL}user/protected`, {
+                    withCredentials: true,
+                    headers: {
+                        Authorization: `Bearer ${user}`,
+                    },
+                })
+                .then((res) => {
+                    if (res.data.user) {
+                        // Stores user info into the state.
+                        setGetUser(res.data.user)
+                        if (res.data.user.role === 'admin') {
+                            setIsRole(true)
+                        } else {
+                            setIsRole(false)
+                        }
+                    }
+                })
+        }
+        if (user) {
+            checkUser()
+        }
+    }, [getUser.role, isRole, navigate, user])
+
+    if (isRole) {
+        return <div>AdminCreateUser</div>
+    }
+
+    return <ErrorPage />
 }
 
 export default AdminCreateUser
