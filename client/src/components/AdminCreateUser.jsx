93645fd43900fba2ed155cbc36e85d011cd10ee7
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-29 14:12:52 +0200

    create static representation for AdminCreateUser component

--- client/src/components/AdminCreateUser.jsx
+++ client/src/components/AdminCreateUser.jsx
@@ -9,6 +9,15 @@ function AdminCreateUser() {
     const user = Cookies.get('access_token')
     const [getUser, setGetUser] = useState([])
     const [isRole, setIsRole] = useState(Boolean)
+    // eslint-disable-next-line no-unused-vars
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    })
+    const { firstName, lastName, email, role, password, confirmPassword } = formData
 
     useEffect(() => {
         const checkUser = async () => {
@@ -38,7 +47,99 @@ function AdminCreateUser() {
     }, [getUser.role, isRole, navigate, user])
 
     if (isRole) {
-        return <div>AdminCreateUser</div>
+        return (
+            <main className="my-5">
+                <section className="container my-3">
+                    <h1>Sign Up</h1>
+                    <form className="row g-3 mt-2">
+                        <div className="col-md-6">
+                            <label htmlFor="firstName" className="form-label">
+                                First name
+                            </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="firstName"
+                                name="firstName"
+                                value={firstName}
+                                placeholder="your first name"
+                            />
+                        </div>
+                        <div className="col-md-6">
+                            <label htmlFor="lastName" className="form-label">
+                                Last name
+                            </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="lastName"
+                                name="lastName"
+                                value={lastName}
+                                placeholder="your last name"
+                            />
+                        </div>
+                        <div className="col-12">
+                            <label htmlFor="email" className="form-label">
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                className="form-control"
+                                name="email"
+                                id="email"
+                                value={email}
+                                placeholder="example@gmail.com"
+                            />
+                        </div>
+                        <div className="col-md-6">
+                            <label htmlFor="password" className="form-label">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                id="password"
+                                name="password"
+                                value={password}
+                                placeholder="Password must be 5"
+                            />
+                        </div>
+                        <div className="col-md-6">
+                            <label htmlFor="confirmPassword" className="form-label">
+                                Confirm Password
+                            </label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                id="confirmPassword"
+                                name="confirmPassword"
+                                value={confirmPassword}
+                                placeholder="confirm your password"
+                            />
+                        </div>
+                        <div className="col-md-12 align-self-center">
+                            <label htmlFor="role" className="form-label">
+                                Choose a role
+                            </label>
+                            <select
+                                className="form-select form-select-md"
+                                aria-label=".form-select-sm example"
+                                id="role"
+                                value={role}
+                            >
+                                <option value="user">User</option>
+                                <option value="admin">Admin</option>
+                            </select>
+                        </div>
+                        <div className="col-12">
+                            <button type="submit" className="btn btn-primary btn-lg">
+                                Sign up
+                            </button>
+                        </div>
+                    </form>
+                </section>
+            </main>
+        )
     }
 
     return <ErrorPage />
