98332a28aa5d939dbec54dd7530cb0f3acf197e9
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-30 22:17:28 +0200

    let user store only one exercise per list

--- backend/routes/userListInfo.js
+++ backend/routes/userListInfo.js
@@ -1,35 +1,39 @@
-const express = require("express");
-const userListInfo = require("../model/userListInfo");
-const userList = require("../model/userList");
-const dotenv = require("dotenv").config();
+const express = require('express');
+const userListInfo = require('../model/userListInfo');
+const userList = require('../model/userList');
+const dotenv = require('dotenv').config();
 const router = express.Router();
 
 //Create ListInfo
-router.post("/createInfo/:id", async (req, res) => {
+router.post('/createInfo/:id', async (req, res) => {
   try {
     const id = req.params.id;
-    const newList = new userListInfo({
-      name: req.body.name,
-      description: req.body.description,
-      bodyPart: req.body.bodyPart,
-      target: req.body.target,
-      exId: req.body.exId,
-      gifUrl: req.body.gifUrl,
-      equipment: req.body.equipment,
+    const exist = await userListInfo.findOne({
       uList: id,
+      exId: req.body.exId,
     });
-    newList
-      .save()
-      .then(
-        res.status(200).json({ message: "Exercise has been added to list" })
-      );
+    if (exist === null) {
+      const newList = new userListInfo({
+        name: req.body.name,
+        exId: req.body.exId,
+        uList: id,
+      });
+      newList
+        .save()
+        .then(
+          res.status(200).json({ message: 'Exercise has been added to list' })
+        );
+    } else {
+      res.status(404).json({ message: 'Exercise is already in list' });
+    }
   } catch (error) {
-    res.status(404).json({ message: "Invalid Information, try again" });
+    res.status(404).json({ message: 'Invalid Information, try again' });
+    console.log(error);
   }
 });
 
 //Read User Lists Information
-router.get("/listInfo/:id", async (req, res) => {
+router.get('/listInfo/:id', async (req, res) => {
   try {
     const id = req.params.id;
     const lInfo = await userListInfo.find({ uList: id });
@@ -40,15 +44,15 @@ router.get("/listInfo/:id", async (req, res) => {
 });
 
 //Delete User Lists Information
-router.delete("/listInfoDelete/:id", async (req, res) => {
+router.delete('/listInfoDelete/:id', async (req, res) => {
   try {
     const id = req.params.id;
     await userListInfo.findByIdAndDelete(id);
     res
       .status(200)
-      .json({ message: "Exercise has now been deletet from list!" });
+      .json({ message: 'Exercise has now been deletet from list!' });
   } catch (error) {
-    res.status(404).json({ message: "Invalid Id!" });
+    res.status(404).json({ message: 'Invalid Id!' });
   }
 });
 
