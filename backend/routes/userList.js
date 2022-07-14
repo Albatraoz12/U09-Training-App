c9d2a4485324f18f8f5d0b1e73c6e190dc82612a
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-07-14 09:55:41 +0200

    add authorization middleware to POST and GET routes in userList.js

--- backend/routes/userList.js
+++ backend/routes/userList.js
@@ -3,29 +3,62 @@ const User = require('../model/user');
 const userList = require('../model/userList');
 const dotenv = require('dotenv').config();
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
-//Crate userList
-router.post('/createList/:id', (req, res) => {
+//Middleweare, authorize users token
+const authorization = (req, res, next) => {
+  // const token = req.cookies.access_token;
+  const token = req.headers.authorization.split(' ')[1];
+  if (!token) {
+    return res.status(403).json({ message: 'You are not Authorized!' });
+  }
   try {
-    const id = req.params.id;
-    const newList = new userList({
-      title: req.body.title,
-      user: id,
-    });
-    newList
-      .save()
-      .then(res.status(200).json({ message: 'new list has been created' }));
+    const data = jwt.verify(token, process.env.SECRET);
+    req.userId = data.id;
+    req.role = data.role;
+    return next();
+  } catch {
+    return res.status(403).json({ message: 'You have no valid token' });
+  }
+};
+//Crate userList
+router.post('/createList/:uid', authorization, (req, res) => {
+  try {
+    const uid = req.params.uid;
+    console.log(req.userId);
+    console.log(uid);
+    if (req.userId === uid) {
+      const newList = new userList({
+        title: req.body.title,
+        user: uid,
+        listRole: req.role,
+      });
+      newList
+        .save()
+        .then(res.status(200).json({ message: 'new list has been created' }));
+    } else {
+      res.status(404).json({ ErrorMessage: 'wrong userId' });
+    }
   } catch (error) {
     console.log(error);
   }
 });
 
 //Get userlists by userID
-router.get('/:id', async (req, res) => {
+router.get('/:uid', authorization, async (req, res) => {
   try {
-    const id = req.params.id;
-    const list = await userList.find({ user: id });
-    res.status(200).json({ message: list });
+    const user = User.findOne({ _id: req.userId });
+    if (user) {
+      const uid = req.params.uid;
+      if (req.userId === uid) {
+        const list = await userList.find({ user: uid });
+        res.status(200).json({ message: list });
+      } else {
+        res.status(404).json({ ErrorMessage: 'Wrong crediantials!' });
+      }
+    } else {
+      res.status(404).json({ ErrorMessage: 'You are not an user!' });
+    }
   } catch (error) {
     res.status(404).json({ message: 'There is no user in the system' });
   }
