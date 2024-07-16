df1e004dfc58c3fb0c40b3f715fad1d701d36f66
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-07-16 21:18:39 +0200

    add more accessibility and code reading

--- client/src/pages/ExercisePage.jsx
+++ client/src/pages/ExercisePage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useState, useEffect, useContext } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import Cookies from 'js-cookie'
@@ -8,13 +9,13 @@ import { deleteSaved, exerciseToList, getExercise, saveExercise } from '../compo
 
 function ExercisePage() {
     const { user, userLists, userSaves } = useContext(UserContext)
-    const params = useParams() // Let developers get access to params
+    const params = useParams()
     const token = Cookies.get('access_token')
     const [errorModal, setErrorModal] = useState(false)
     const [errorMessage, setErrorMessage] = useState('')
-    const [exercise, setExercise] = useState([]) // Stores the exercise data
-    const [isLoggedIn, setIsLoggedIn] = useState(false) // checks if the user is logged in or not
-    const [isSaved, setIsSaved] = useState(false) // If set to true, the user has then already liked/saved it
+    const [exercise, setExercise] = useState({})
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [isSaved, setIsSaved] = useState(false)
     const [formData, setFormData] = useState({
         name: '',
         exId: '',
@@ -22,47 +23,40 @@ function ExercisePage() {
 
     useEffect(() => {
         async function fetchUserData() {
-            setIsLoggedIn(true)
+            setIsLoggedIn(!!user)
             if (userSaves && userSaves.length > 0) {
-                userSaves.map((saves) => {
-                    if (saves.exId === params.id) {
+                userSaves.forEach((save) => {
+                    if (save.exId === params.id) {
                         setIsSaved(true)
                     }
-                    return saves
                 })
             }
         }
+
         const fetchExercise = async () => {
             const currentEx = await getExercise(params.id)
             setExercise(currentEx)
-            if (exercise.name && exercise.id) {
-                setFormData({
-                    name: exercise.name,
-                    exId: exercise.id,
-                })
-            }
+            setFormData({
+                name: currentEx.name,
+                exId: currentEx.id,
+            })
         }
+
         if (user) {
             fetchUserData()
         }
         fetchExercise()
+    }, [user, userSaves, params.id])
 
-        // Disable this line because of not neeeding the dependensis
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [params.id, exercise.name, exercise.id])
-
-    // function will run when user clicks on Like button
     const save = async (data) => {
         const saved = await saveExercise(user.id, token, data)
         if (saved.message) window.location.reload()
     }
 
-    // When user clicks on a list from the dropdown, the exercise will be saved into that list
     const saveList = async (id) => {
         const saveToList = await exerciseToList(id, token, formData)
         if (saveToList.message) {
-            // eslint-disable-next-line no-alert
-            alert('success')
+            alert('Success')
         } else {
             setErrorMessage(saveToList.errorMessage)
             setErrorModal(true)
@@ -77,12 +71,16 @@ function ExercisePage() {
                 <article className="px-5">
                     <h2>About</h2>
                     <p className="px-2">
-                        {exercise.name} can increase your blood flow, try this exercise to
-                        strengthen your {exercise.target}
+                        {exercise.name} can increase your blood flow. Try this exercise to
+                        strengthen your {exercise.target}.
                     </p>
                 </article>
                 <div className="col-12 col-xl-4 mx-auto">
-                    <img className="card-img-top" src={exercise.gifUrl} alt="Exercise Gif" />
+                    <img
+                        className="card-img-top"
+                        src={exercise.gifUrl}
+                        alt={`${exercise.name} GIF`}
+                    />
                 </div>
                 {errorModal && (
                     <div className="d-flex align-items-center justify-content-center">
@@ -90,20 +88,18 @@ function ExercisePage() {
                     </div>
                 )}
                 <section className="my-2">
-                    {/* Message to display if user is logged in or not */}
                     {isLoggedIn ? (
-                        <h2>Save this Exercise or save it into a list</h2>
+                        <h2>Save this exercise or save it into a list</h2>
                     ) : (
                         <h2>
                             <Link className="text-white" to="/signin">
-                                {' '}
                                 Login here
                             </Link>{' '}
                             to save exercise
                         </h2>
                     )}
                     <div className="d-flex justify-content-center gap-2">
-                        {isLoggedIn ? (
+                        {isLoggedIn && (
                             <>
                                 {isSaved ? (
                                     <button
@@ -134,6 +130,7 @@ function ExercisePage() {
                                         id="dropdownMenuButton1"
                                         data-bs-toggle="dropdown"
                                         aria-expanded="false"
+                                        aria-haspopup="true"
                                     >
                                         Your Lists
                                     </button>
@@ -141,27 +138,21 @@ function ExercisePage() {
                                         className="dropdown-menu"
                                         aria-labelledby="dropdownMenuButton1"
                                     >
-                                        {userLists.map((list, index) => {
-                                            return (
-                                                // eslint-disable-next-line react/no-array-index-key
-                                                <li key={index}>
-                                                    <button
-                                                        type="button"
-                                                        className="dropdown-item"
-                                                        onClick={() => {
-                                                            // eslint-disable-next-line no-underscore-dangle
-                                                            saveList(list._id)
-                                                        }}
-                                                    >
-                                                        {list.title}
-                                                    </button>
-                                                </li>
-                                            )
-                                        })}
+                                        {userLists.map((list) => (
+                                            <li key={list._id}>
+                                                <button
+                                                    type="button"
+                                                    className="dropdown-item"
+                                                    onClick={() => saveList(list._id)}
+                                                >
+                                                    {list.title}
+                                                </button>
+                                            </li>
+                                        ))}
                                     </ul>
                                 </div>
                             </>
-                        ) : null}
+                        )}
                     </div>
                 </section>
             </section>
